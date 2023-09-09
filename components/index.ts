860893f9ba2dfd7bd619e950326a817692a2eda3
Albin <albinbinoy006@gmail.com>
2023-09-09 14:45:39 +0530

    first commit

---
+++ components/index.ts
@@ -0,0 +1,23 @@
+import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import SearchManufacturer from "./SearchManufacturer";
+import CarDetails from "./CarDetails";
+import ShowMore from "./ShowMore";
+import CarCard from "./CarCard";
+
+export {
+    Hero,
+    CustomButton,
+    Navbar,
+    Footer,
+    SearchBar,
+    CustomFilter,
+    SearchManufacturer,
+    CarDetails,
+    CarCard,
+    ShowMore
+}
